c47c13ec3cbd2642ceba3c2657d9464769967756
Engjëll Abazaj <128191756+yourboyangel@users.noreply.github.com>
2025-04-26 23:02:15 +0200

    finished liked songs functionality

--- music-streaming/app/search/components/SearchContent.tsx
+++ music-streaming/app/search/components/SearchContent.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import LikeButton from "@/components/LikeButton";
 import MediaItem from "@/components/MediaItem";
 import { Song } from "@/types";
 
@@ -38,6 +39,7 @@ const SearchContent: React.FC<SearchContentProps> = ({
                             data={song}
                         />
                     </div>
+                    <LikeButton songId={song.id} />
                 </div>
             ))}
         </div>
